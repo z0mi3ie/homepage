c3a02fe4106a1d339b1f9b2e4cd6670d94859ca7
Kyle Vickers <kylevickers@gmail.com>
2023-02-14 04:45:23 -0500

    added linkedin link and updated page title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,5 @@
 import React from "react"
 
-//import logo from "../images/headshot.jpg"
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -18,6 +16,7 @@ const Header = () => {
                 <h2 className="medialinks">
                     <a className="iconlink" href="http://www.github.com/z0mi3ie"><FontAwesomeIcon className="fa-fw" icon={['fab', 'github']} /></a>
                     <a className="iconlink" href="https://www.instagram.com/mr_trashcans"><FontAwesomeIcon className="fa-fw" icon={['fab', 'instagram']} /></a>
+                    <a className="iconlink" href="https://www.linkedin.com/in/kylevickers89/"><FontAwesomeIcon className="fa-fw" icon={['fab', 'linkedin']} /></a>
                 </h2>
             </div>
         </header>
